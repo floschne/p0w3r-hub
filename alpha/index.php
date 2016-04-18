fc32208a34e6b91d5d6b90260e9b54ee5dd82ccb
Florian Schneider <Flows@web.de>
2016-04-18 23:13:48 +0200

    basic structure created

---
+++ alpha/index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>p0w3r-hub alpha</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+        <!-- w3.css -->
+        <link rel="stylesheet" type="text/css" href="css/w3.css" media="all" />
+        <!-- FONT AWESOME -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <!-- gFonts-->
+        <link href='https://fonts.googleapis.com/css?family=Anonymous+Pro' rel='stylesheet' type='text/css'>
+        <!-- jQuery-->
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    </head>
+
+    <body class="w3-dark-grey">
+        <nav class="w3-topnav w3-xlarge w3-row  w3-center w3-card-2 w3-top w3-left-align w3-light-green">
+                <a href="#" class="w3-col m2"><span><i class="fa fa-home">&nbsp;</i> p0w3r-hub</span></a>
+                <a href="#" class="w3-col m2"><span><i class="fa fa-home">&nbsp;</i> Home</span></a>
+                <a href="#" class="w3-col m2"><span><i class="fa fa-cog">&nbsp;</i>All Cards</span></a>
+                <a href="#" class="w3-col m2"><span><i class="fa fa-cog">&nbsp;</i>Card Repository</span></a>
+                <a href="#" class="w3-col m2"><span><i class="fa fa-cog">&nbsp;</i>Deck Builder</span></a>
+                <a href="#" class="w3-col m2"><span><i class="fa fa-cog">&nbsp;</i>Account Settings</span></a>
+        </nav>
+        <div class="w3-container w3-padding-hor-64">
+            <div id="cardInfo" class="w3-col w3-margin-right" style="width:20%">
+                <div id="cardImg" class="w3-display-container w3-center">
+                    <img src="gfx/card.jpg" alt="card demo" class="w3-round-large w3-card-8" style="width:75%">
+                </div>
+                <hr/>
+                <div id="cardProperties" class="w3-container">
+                    
+                </div>
+            </div>
+            <div id="cardList" class="w3-rest w3-card-8">
+                <table class="w3-table w3-striped w3-hoverable w3-text-black">
+                    <thead>
+                        <tr class="w3-green">
+                          <th>First Name</th>
+                          <th>Last Name</th>
+                          <th>Points</th>
+                        </tr>
+                    </thead>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                    <tr>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                    </tr>
+                    <tr>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                    </tr>
+                    <tr>
+                      <td>Adam</td>
+                      <td>Johnson</td>
+                      <td>67</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <footer class="w3-container w3-light-green w3-bottom w3-center">
+            <h6>footer</h6>
+        </footer>
+    </body>
+
+</html>
